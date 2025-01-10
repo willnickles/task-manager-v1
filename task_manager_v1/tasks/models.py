82acacc37abf1fae28d423799838a75aaf82b350
wnickles <wnickles@uiowa.edu>
2025-01-10 12:09:10 -0600

    first commit

---
+++ task_manager_v1/tasks/models.py
@@ -0,0 +1,12 @@
+
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+# Create your models here.
